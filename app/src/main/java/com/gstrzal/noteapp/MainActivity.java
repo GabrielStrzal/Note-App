084c0a65eeead45f75049d35f70e000daa32e808
Gabriel Strzalkowski Moreira <gmoreira@gcosi.local>
2020-03-12 10:34:34 -0400

    adding adapter and data to screen

--- app/src/main/java/com/gstrzal/noteapp/MainActivity.java
+++ app/src/main/java/com/gstrzal/noteapp/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
+import com.gstrzal.noteapp.adapter.NoteAdapter;
 import com.gstrzal.noteapp.entity.Note;
 import com.gstrzal.noteapp.viewModel.NoteViewModel;
 
@@ -22,12 +24,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
         noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(@Nullable List<Note> notes) {
-                //update RecyclerView
-                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+                adapter.setNotes(notes);
             }
         });
     }
