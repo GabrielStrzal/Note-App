942aa20ae04c36e0192bda0958b75771f0e07e12
Gabriel Strzalkowski Moreira <gmoreira@gcosi.local>
2020-03-12 09:27:06 -0400

    initial setup

---
+++ app/src/main/java/com/gstrzal/noteapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.gstrzal.noteapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.gstrzal.noteapp.entity.Note;
+import com.gstrzal.noteapp.viewModel.NoteViewModel;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(@Nullable List<Note> notes) {
+                //update RecyclerView
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
